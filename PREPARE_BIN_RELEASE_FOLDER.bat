2b8152b74957f684482d1d65a85b17c124dfd67c
Christoph Lassner <Christoph.Lassner@student.uni-augsburg.de>
2011-10-04 10:29:48 +0200

    Readme updated.

--- PREPARE_BIN_RELEASE_FOLDER.bat
+++ PREPARE_BIN_RELEASE_FOLDER.bat
@@ -1,3 +1,4 @@
 copy /Y .\README /A .\ApplicationSetup\Release /A
 copy /Y .\Documentation\README.pdf /B .\ApplicationSetup\Release /B
 copy /Y .\KinectAET\Resources\Licenses\gpl-3.0.txt .\ApplicationSetup\Release
+PAUSE
